a63e922375434b9d8f7effffe14845d240b430f9
Kjetil Ørbekk <kjetil.orbekk@gmail.com>
2012-01-10 15:38:31 +0100

    Remove rpctest project.
    Apache xmlrpc doesn't work on Android because of a mismatch in Java core
    classes [1]. Remove rpctest project.
    
    In my master's project, jsonrpc4j will be used instead of xmlrpc.
    
    [1] http://code.google.com/p/android/issues/detail?id=9493

--- rpctest/src/main/java/com/orbekk/Calculator.java
+++
@@ -1,5 +0,0 @@
-package com.orbekk;
-
-interface Calculator {
-    int add(int x, int y);
-}
--- rpctest/src/main/java/com/orbekk/CalculatorImpl.java
+++
@@ -1,7 +0,0 @@
-package com.orbekk;
-
-public class CalculatorImpl implements Calculator {
-    public int add(int x, int y) {
-        return x + y;
-    }
-}
