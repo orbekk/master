ec2e44fffe208e8035497d00c82cbe2405db150a
Kjetil Ørbekk <kjetil.orbekk@gmail.com>
2012-01-15 18:27:31 +0100

    Switch parameter order for consistency.

--- same/src/main/java/com/orbekk/same/ClientService.java
+++ same/src/main/java/com/orbekk/same/ClientService.java
@@ -3,5 +3,5 @@ package com.orbekk.same;
 public interface ClientService {
     void notifyNetwork(String networkName, String masterUrl);
     
-    void setState(String component, long revision, String data);
+    void setState(String component, String data, long revision);
 }
