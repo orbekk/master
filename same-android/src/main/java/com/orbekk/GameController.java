3f5c54d56de87ac2afc51222dfa2373b5bb8238e
Kjetil Ørbekk <kjetil.orbekk@gmail.com>
2012-01-19 20:57:34 +0100

    Start implementing GameController.
    – GameController will manage the game state with more than one player,
    and communicating its state with the GUI and the other players.

--- same-android/src/main/java/com/orbekk/GameController.java
+++ same-android/src/main/java/com/orbekk/GameController.java
@@ -1,5 +1,32 @@
 package com.orbekk;
 
-public class GameController {
+import java.util.ArrayList;
+import java.util.List;
 
+import android.graphics.Paint;
+
+public class GameController {
+    private List<Player> players = new ArrayList<Player>();
+    private Player localPlayer;
+    
+    public static class Player {
+        public Paint color;
+        public int posX;
+        public int posY;
+    }
+    
+    public GameController(Player localPlayer) {
+        this.localPlayer = localPlayer;
+    }
+    
+    public Player getLocalPlayer() {
+        return localPlayer;
+    }
+    
+    public List<Player> getRemotePlayers() {
+        return players;
+    }
+    
+    
+    
 }
