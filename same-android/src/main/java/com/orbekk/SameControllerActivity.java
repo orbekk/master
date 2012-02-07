4e2adf917b0a6112c8d0db1a7afdf937866f4a3d
Kjetil Ørbekk <kjetil.orbekk@gmail.com>
2012-02-07 14:06:22 +0100

    Remove unneeded documentation.
    Remove documentation from Hello Android project.

--- same-android/src/main/java/com/orbekk/SameControllerActivity.java
+++ same-android/src/main/java/com/orbekk/SameControllerActivity.java
@@ -49,13 +49,7 @@ public class SameControllerActivity extends Activity {
     public void doneClicked(View unused) {
         finish();
     }
-    
-    /**
-     * Called when the activity is first created.
-     * @param savedInstanceState If the activity is being re-initialized after 
-     * previously being shut down then this Bundle contains the data it most 
-     * recently supplied in onSaveInstanceState(Bundle). <b>Note: Otherwise it is null.</b>
-     */
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
