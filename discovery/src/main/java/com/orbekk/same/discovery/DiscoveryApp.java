8bbb0f4ce2d80821893b2c32b36bdedf914ccee2
Kjetil Ørbekk <kjetil.orbekk@gmail.com>
2012-03-02 14:15:10 +0100

    Implement discovery service.
    No main class yet.

---
+++ discovery/src/main/java/com/orbekk/same/discovery/DiscoveryApp.java
@@ -0,0 +1,13 @@
+package com.orbekk.same.discovery;
+
+import com.orbekk.same.http.ServerContainer;
+
+public class DiscoveryApp {
+    public static final int DISCOVERY_PORT = 15072;
+    
+    public static void main(String[] args) {
+        /**
+         * TODO: Need to move DiscoveryService to same project.
+         */
+    }
+}
--- discovery/src/main/java/com/orbekk/same/discovery/DiscoveryServiceImpl.java
+++ discovery/src/main/java/com/orbekk/same/discovery/DiscoveryServiceImpl.java
@@ -1,5 +1,41 @@
 package com.orbekk.same.discovery;
 
-public class DiscoveryServiceImpl {
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class DiscoveryServiceImpl implements DiscoveryService {
+    public final static long EXPIRE_TIME = 15 * 60l * 1000;  // 15 minutes
+    List<NetworkEntry> networkList = new ArrayList<NetworkEntry>();
     
+    synchronized void cleanNetworkList() {
+        long expiredTime = System.currentTimeMillis() - EXPIRE_TIME;
+        for (Iterator<NetworkEntry> it = networkList.iterator(); it.hasNext();) {
+            NetworkEntry e = it.next();
+            if (e.hasExpired(expiredTime)) {
+                it.remove();
+            }
+        }
+    }
+
+    @Override
+    public List<String> getNetworks() throws Exception {
+        cleanNetworkList();
+        List<String> networks = new ArrayList<String>();
+        for (NetworkEntry e : networkList) {
+            networks.add(e.networkName);
+            networks.add(e.masterUrl);
+        }
+        return networks;
+    }
+
+    @Override
+    public void registerNetwork(String networkName, String masterUrl)
+            throws Exception {
+        cleanNetworkList();
+        NetworkEntry entry = new NetworkEntry(networkName, masterUrl);
+        entry.register(System.currentTimeMillis());
+        networkList.remove(entry);
+        networkList.add(entry);
+    }
 }
