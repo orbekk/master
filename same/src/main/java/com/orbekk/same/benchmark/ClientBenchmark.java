45622f6c0d98c4b21375374cd748bae8e0ca1daf
Kjetil Ørbekk <kjetil.orbekk@gmail.com>
2012-04-09 12:06:22 +0200

    Add (non-Android) protobuf benchmark code.

---
+++ same/src/main/java/com/orbekk/same/benchmark/ClientBenchmark.java
@@ -0,0 +1,83 @@
+package com.orbekk.same.benchmark;
+
+import java.util.concurrent.CountDownLatch;
+
+import com.google.protobuf.RpcCallback;
+import com.orbekk.protobuf.Rpc;
+import com.orbekk.protobuf.RpcChannel;
+import com.orbekk.same.benchmark.Example.Data;
+
+public class ClientBenchmark {
+    private final Example.Service service;
+    private final int warmupIterations;
+    private final int iterations;
+    
+    public static void benchmark(String host, int port, int warmupIterations,
+            int iterations) throws InterruptedException {
+        RpcChannel channel = null;
+        try {
+            channel = RpcChannel.create(host, port);
+            Example.Service service = Example.Service.newStub(channel);
+            ClientBenchmark benchmark = new ClientBenchmark(
+                    service, warmupIterations, iterations);
+            benchmark.benchmark();
+        } finally {
+            if (channel != null) {
+                channel.close();
+            }
+        }
+    }
+    
+    public ClientBenchmark(Example.Service service,
+            int warmupIterations, int iterations) {
+        this.service = service;
+        this.warmupIterations = warmupIterations;
+        this.iterations = iterations;
+    }
+    
+    private void runBenchmark(int iterations) throws InterruptedException {
+        final CountDownLatch finished =
+                new CountDownLatch(iterations);
+        
+        for (int i = 0; i < iterations; i++) {
+            Example.Data request = Example.Data.newBuilder()
+                    .setArg1(i).build();
+            Rpc rpc = new Rpc();
+            service.methodA(rpc, request, new RpcCallback<Example.Data>() {
+                @Override
+                public void run(Data ignored) {
+                    finished.countDown();
+                }
+            });
+        }
+        
+        finished.await();
+    }
+    
+    public void benchmark() throws InterruptedException {
+        long warmupStart = System.currentTimeMillis();
+        runBenchmark(warmupIterations);
+        long warmupFinished = System.currentTimeMillis();
+        System.out.println("Warmup: " + warmupIterations + " in " +
+                (warmupFinished - warmupStart) + "ms. ");
+        long start = System.currentTimeMillis();
+        runBenchmark(iterations);
+        long finished = System.currentTimeMillis();
+        System.out.println("Benchmark: " + iterations+ " in " +
+                (finished - start) + "ms. ");
+    }
+    
+    public static void main(String[] args) {
+        if (args.length < 2) {
+            System.err.println("Usage: ClientBenchmark <host> <port>");
+            System.exit(1);
+        }
+        String host = args[0];
+        int port = Integer.valueOf(args[1]);
+        try {
+            benchmark(host, port, 1000, 10000);
+        } catch (InterruptedException e) {
+            System.out.println("Benchmark failed.");
+        }
+    }
+}
---
+++ same/src/main/java/com/orbekk/same/benchmark/ExampleServer.java
@@ -0,0 +1,42 @@
+package com.orbekk.same.benchmark;
+
+import org.apache.log4j.Logger;
+
+import com.google.protobuf.RpcCallback;
+import com.google.protobuf.RpcController;
+import com.orbekk.protobuf.SimpleProtobufServer;
+import com.orbekk.same.benchmark.Example.Data;
+
+public class ExampleServer {
+    private final static Logger logger =
+            Logger.getLogger(ExampleServer.class);
+    private volatile SimpleProtobufServer server;
+    
+    class ServiceImpl extends Example.Service {
+        @Override
+        public void methodA(RpcController controller, Data request, RpcCallback<Data> done) {
+            Data response = Data.newBuilder()
+                .setMessage(request.getMessage())
+                .setArg1(request.getArg1())
+                .setArg2(request.getArg2())
+                .build();
+            done.run(response);
+        }
+    }
+    
+    public void runServer(int port) {
+        server = SimpleProtobufServer.create(port);
+        server.registerService(new ServiceImpl());
+        server.start();
+        logger.info("Running SimpleProtobufServer on port " + server.getPort());
+    }
+    
+    public void stopServer() throws InterruptedException {
+        server.interrupt();
+    }
+    
+    public static void main(String[] args) {
+        ExampleServer server = new ExampleServer();
+        server.runServer(12000);
+    }
+}
